5003965ae4e8ce7966a361b982c41fe425df9c58
Sam Lloyd <sam.thomas.lloyd@gmail.com>
2021-09-07 16:39:08 +0100

    sprite added and centered

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let app = new PIXI.Application({ width: 640, height: 360 });
 document.body.appendChild(app.view);
 
-let sprite = PIXI.Sprite.from("images/Player1.png");
+let player1 = PIXI.Sprite.from("images/Player1.png");
 
-app.stage.addChild(sprite);
+player1.width = 120; // sets sprite character image size
+player1.height = 120;
+
+/*positioning of player1 sprite*/
+player1.anchor.set(0.5); //sets where in sprite anchor point is
+player1.x = app.view.width /2; // centers sprite in VP 
+player1.y = app.view.height /2;
+
+
+app.stage.addChild(player1);
